61bdff2b97855c776e71d82b3ebb79b86c52a98f
daisydaniela36 <33319268+daisydaniela36@users.noreply.github.com>
2021-04-18 17:34:15 -0400

    conectar botones
    se conectaron los botones para que se redirijan de una actividad a otra

--- DeliveryApp/app/src/main/java/com/example/deliveryapp/ResetPasswordActivity.java
+++ DeliveryApp/app/src/main/java/com/example/deliveryapp/ResetPasswordActivity.java
@@ -2,13 +2,30 @@ package com.example.deliveryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ResetPasswordActivity extends AppCompatActivity {
+
+    TextView txt_RVolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+
+        txt_RVolver = (TextView) findViewById(R.id.txt_Volver);
+
+
+        txt_RVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResetPasswordActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
